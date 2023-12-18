b21b837dd9a470613b92612197397e8fb1bbdbda
Leandro Vilar <leandrovilar@outlook.com>
2023-12-18 20:51:40 -0300

    variaveis

---
+++ 004 Variáveis/001 entendendo/index.js
@@ -0,0 +1,24 @@
+/*
+    var - Ele é utilizado globalmente
+    let - Ele é gerado dentro de um escopo
+    const - Ele é gerado dendto de um escopo porém podemos armazenar dados apenas 1x
+*/
+
+var cachorro = "Rabito";
+console.log(cachorro);
+
+cachorro = "Golias"
+console.log(cachorro);
+
+let lanche = "bolovo";
+console.log(lanche);
+
+lanche = "Misto quente";
+console.log(lanche);
+
+const nome = "Leandro";
+console.log(nome);
+
+nome = "Leandro Vilar";
+console.log(nome);
+
