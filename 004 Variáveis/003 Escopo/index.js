119c80577e445ffa982a00d4fba033b90063bd27
Leandro Vilar <leandrovilar@outlook.com>
2023-12-18 21:11:16 -0300

    escopo

---
+++ 004 Variáveis/003 Escopo/index.js
@@ -0,0 +1,12 @@
+/*
+Escopo:
+Determina quais são os dados que podem ser acessados em uma determinada parte do código
+*/
+
+
+{
+    let nome = "Leandro";
+   // console.log(nome);
+}
+
+console.log(nome); // nao consegue achar a variavel fora do console
