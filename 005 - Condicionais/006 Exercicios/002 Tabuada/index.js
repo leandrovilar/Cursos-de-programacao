8fa9051a42319a6a6301585809977ae0db72678c
Leandro Vilar <leandrovilar@outlook.com>
2024-01-15 14:56:51 -0300

    Tabulacao

---
+++ 005 - Condicionais/006 Exercicios/002 Tabuada/index.js
@@ -0,0 +1,22 @@
+/*
+Precisamos mostrar a tabuada no console de 0 ate 10 neste seguinte formamdo:
+
+multiplicador x nuemro = resultado
+multiplicador x 0 = resultado
+multiplicador x 1 = resultado
+multiplicador x 2 = resultado
+multiplicador x 3 = resultado
+
+Obs.: Nao podemso criar vairos consoles.log (multiplicador x 3 = resultado)
+
+*/
+
+const multiplicador =7;
+
+// let i = 0
+
+for (let i = 0; i <= 10; i++){
+    const calc = multiplicador * i;
+    console.log (`${multiplicador} x ${i} = ${calc}`);
+
+}
