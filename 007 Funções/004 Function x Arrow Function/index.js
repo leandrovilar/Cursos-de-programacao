2a6c3a0434dc7c5be6ffa519ade56c9dc17237cb
Leandro Vilar <leandrovilar@outlook.com>
2024-01-18 21:44:01 -0300

    Function x Arrow Funcitio

--- 007 Funções/004 Function x Arrow Function/index.js
+++ 007 Funções/004 Function x Arrow Function/index.js
@@ -34,38 +34,36 @@ function nomeFuncHosting() {
 
 // arguments
 
-    /* Funciona */
-function nomeFuncArguments(){
+/* Funciona */
+function nomeFuncArguments() {
     return arguments;
 }
 console.log(nomeFuncArguments("arguments"))
 
 
-    /* nao funciona Arrow
-   
-    const nomeFuncArrowArguments = () => {
-    return arguments;
-   }
-   console.log(nomeFuncArrowArguments())
-
-   */
-   
-
-
-/*
-5 Nao pode ser invocadaco New
-
-    function novaFunc() {
-        return console.log (123);
-    }
-    new novaFunc();
-
-    const novaFuncArrow = () => {
-        return console.log(123);
-    }
-    new novaFuncArrow();
+/* nao funciona Arrow
+ 
+const nomeFuncArrowArguments = () => {
+return arguments;
+}
+console.log(nomeFuncArrowArguments())
 
 */
+
+
+
+
+//5 Nao pode ser invocadaco New
+
+function novaFunc() {
+    //       return console.log (123);
+}
+new novaFunc();
+
+const novaFuncArrow = () => {
+    //       return console.log(123);
+}
+new novaFuncArrow();
 
 /*
 6 - Contexto
@@ -75,10 +73,34 @@ Isso significa que arrow functions herdam o contexto local de onde foi declarado
 
 Se ele nao estiver associado ao ninguem na chamada, ele assumirá this automaticamente como o contexto global, que no caso dos navegadores é window
 */
+const lanches = {
+    cardapio: [
+        { nome: "x-salada", preço: "R$ 25" },
+        { nome: "x-bolovo", preço: "R$ 30" },
+    ],
 
+    meuPedidoFunc: function (select) {
+        return console.log(this.cardapio[select]);
+    },
 
+    meuPedidoFuncTimeOut: function () {
+        setTimeout(
+            function () {
+                console.log(this.cardapio);
+                console.log(this);
+            }.bind(this),
+            1000
+        );
+    },
 
-
+    meuPedidoArrowFunc: (select) => {
+        this.cardapio = [
+            { nome: "x-salada", preço: "R$ 45" },
+            { nome: "x-bolovo", preço: "R$ 50" },
+        ];
+        return console.log(this.cardapio[select]);
+    },
+};
 
 
 
@@ -86,4 +108,11 @@ Se ele nao estiver associado ao ninguem na chamada, ele assumirá this automatic
 7 - Constructor 
 Arrow functions nao podem ser contructors, entao nao e possivel usar o operador new com a mesma.
 */
+class newFunc {
+    constructor(nome) {
+        this.nome = nome;
+    }
+}
 
+const a = new newFunc("Leandro Vilar");
+console.log(a.nome);
