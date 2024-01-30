cdea8c6d5bfc00f30b017b21144c7defabdf9556
Leandro Vilar <leandrovilar@outlook.com>
2024-01-30 11:05:21 -0300

    Constructor

---
+++ 000 DIO/003 Classe/index.js
@@ -0,0 +1,48 @@
+// Auda 1 - Classe e instância
+
+// Classe
+// class Pessoa {
+//     nome;
+//     idade;
+
+//     descrever() {
+//         console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
+//     }
+// }
+// // instância 
+// const leandro = new Pessoa();
+// leandro.nome = `Leandro Vilar`;
+// leandro.idade = 43;
+
+// const marlene = new Pessoa();
+// marlene.nome = `Marlene Rodrigues`;
+// marlene.idade = `44`;
+
+// console.log(leandro);
+// console.log(marlene);
+
+// leandro.descrever();
+// marlene.descrever();
+
+
+// Aula 2 - Como criar instacias com contrutor 
+
+class Pessoa {
+    nome;
+    idade;
+    anoDeNascimento;
+
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.anoDeNascimento = 2024 - idade;
+    }
+
+    descrever() {
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
+    }
+}
+
+const leandro = new Pessoa(`Leandro Vilar`, 43);
+console.log(leandro);
+leandro.descrever();
