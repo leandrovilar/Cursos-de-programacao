826499d3df92691713e6d9e6eeb3bc0fdd941e89
Leandro Vilar <leandrovilar@outlook.com>
2024-01-30 11:35:48 -0300

    função

---
+++ 000 DIO/004 Funções recebendo objeto/index.js
@@ -0,0 +1,34 @@
+// Aula 1 - Funçoes recebendo objeto
+
+class Pessoa {
+    nome;
+    idade;
+    anoDeNascimento;
+
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.anoDeNascimento = 2024 - idade;
+    }
+
+    descrever() {
+        console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade}`);
+    }
+}
+
+// função
+function compararPessoa(p1, p2) {
+    if (p1.idade > p2.idade) {
+        console.log(`${p1.nome} é mais velho(a) que ${p2.nome}`);
+    } else if (p2.idade > p1.idade) {
+        console.log(`${p2.nome} é mais velho(a) que ${p1.nome}`);
+    } else {
+        console.log(`${p1.nome} e ${p2.nome} tem a mesma idade`);
+    }
+}
+
+const leandro = new Pessoa (`Leadro`, 43);
+const marlene = new Pessoa (`Marlene`, 44);
+
+compararPessoa(leandro, marlene);
+
