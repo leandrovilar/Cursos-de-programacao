4149a5db1669426d8ef3b08f22533969e117c496
Leandro Vilar <leandrovilar@outlook.com>
2024-02-01 09:13:21 -0300

    Ordenar e reordenar valores

--- 000 Udemy/Array a Fundo/009 Ordenando e Revertendo valores/index.js
+++ 000 Udemy/Array a Fundo/009 Ordenando e Revertendo valores/index.js
@@ -3,6 +3,13 @@
 // console.log(stringArray.sort()); // ordena de frente pra traz
 // console.log (stringArray.reverse()); // ordena de traz pra frente
 
+/*
+sort((a, b) => a - b));
+se retornar um valor negativo, o valor de "a" será ordenado antes de "b".
+se retornar 0, a ordenaçao de "a" e "b" não mudará.
+se retornar um valor positivo, o valor em "b" será ordenado antes de "a".
+*/
+
 const numberArray = [10, 20, 30, 40, 50, 1000, 1, 55, 15];
 
 console.log(numberArray.sort((a, b) => a - b));
@@ -24,12 +31,12 @@ const objArray = [
     },
     {
         nome: "José Vilar"
-    }, 
+    },
     {
         nome: "Pedro Vilar"
     },
 ];
 
-console.log (objArray.sort((a,b) => a.nome.localeCompare(b.nome)));
-console.log (objArray.sort((a,b) => b.nome.localeCompare(a.nome)));
-console.log (objArray.sort((a,b) => a.nome.localeCompare(b.nome)).reverse());
+console.log(objArray.sort((a, b) => a.nome.localeCompare(b.nome)));
+console.log(objArray.sort((a, b) => b.nome.localeCompare(a.nome)));
+console.log(objArray.sort((a, b) => a.nome.localeCompare(b.nome)).reverse());
