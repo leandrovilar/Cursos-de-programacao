b94414ecb21756968d11a4ea99595df2a4eacbe5
Leandro Vilar <leandrovilar@outlook.com>
2024-02-20 11:55:56 -0300

    ententdendo o for

---
+++ 000 DIO/06 Lista (arrays)/index.js
@@ -0,0 +1,34 @@
+//Lista de array
+
+// const aluno =['João', 'Victor', 'Marina'];
+
+// aluno.push ('Renan');
+// aluno[4] = 'Vinícius';
+
+// console.log (aluno);
+// console.log (aluno.pop()); // tira o últmo dado
+// console.log (aluno.shift()); // tira o primeiro dado
+// console.log (aluno);
+
+
+// Estrutura de repetição
+
+// const notas = [];
+
+// notas.push(5);
+// notas.push(7);
+// notas.push(8);
+// notas.push(2);
+// notas.push(5);
+
+// console.log (notas.length); // mostra o tamanho da lista
+
+// Entendendo estrutura de repetição dinãmica
+
+const nome = 'Leandro Braga Vilar'
+
+for (let i = 0; i < nome.length; i++) {
+    const letra = nome[i];
+    console.log(letra);
+
+}
