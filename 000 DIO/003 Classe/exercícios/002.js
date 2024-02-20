3091a83c30bcae1bb155746be758ad026b2eba24
Leandro Vilar <leandrovilar@outlook.com>
2024-02-20 10:11:29 -0300

    classificar IMC

--- 000 DIO/003 Classe/exercícios/002.js
+++ 000 DIO/003 Classe/exercícios/002.js
@@ -12,16 +12,32 @@ class Pessoa {
     nome;
     peso;
     altura;
-constructor (nome, peso, altura){
-    this.nome = nome;
-    this.peso = peso;
-    this.altura = altura;
-};
-calcularImc(){
-    return this.peso / (this.altura * this.altura);  // Calculadora de IMC
-}
-}
-const jose = new Pessoa ("José", 70, 1.75); //Dados de José
+    constructor(nome, peso, altura) {
+        this.nome = nome;
+        this.peso = peso;
+        this.altura = altura;
+    };
+    calcularImc() {
+        return this.peso / (this.altura * this.altura);  // Calculadora de IMC\
+    }
+    classificarImc() {
+        const imc = this.calcularImc();
+        if (imc < 18.5) {
+            return (" Abaixo do peso")
+        } else if (imc >= 18.5 && imc < 25) {
+            return ("Peso Nomal");
+        } else if (imc >= 25 && imc < 30) {
+            return ("Acima Obeso");
+        } else if (imc >= 30 && imc < 40) {
+            return ("obeso");
+        } else {
+            return ("Obesidade Grave");
+        }
 
-console.log (jose); // Dados de José
-console.log (jose.calcularImc()); // Calculadora de IMC
+    }
+}
+const jose = new Pessoa("José", 70, 1.75); //Dados de José
+
+console.log(jose); // Dados de José
+console.log(jose.calcularImc()); // Calculadora de IMC
+console.log(jose.classificarImc());
