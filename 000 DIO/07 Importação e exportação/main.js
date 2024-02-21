049e7964c738df7a10f6827591770a5913af8f81
Leandro Vilar <leandrovilar@outlook.com>
2024-02-21 14:13:30 -0300

    destructuring

--- 000 DIO/07 Importação e exportação/main.js
+++ 000 DIO/07 Importação e exportação/main.js
@@ -1,3 +1,7 @@
-const funcoes = require('./funcoes-auxiliares');
+// const funcoes = require('./funcoes-auxiliares');
+// console.log(funcoes.gets());
 
-console.log(funcoes.gets());
+
+//Destructuring 
+const {gets, print} = require('./funcoes-auxiliares');
+print(gets());
