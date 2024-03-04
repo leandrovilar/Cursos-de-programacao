225fdc63003f7774791ea29a502143d76159c405
Leandro Vilar <leandrovilar@outlook.com>
2024-03-04 10:02:59 -0300

    seletor DOM

---
+++ 000 Udemy/010 DOM - Selecionando/007 Qual devo usar para selecionar com o DOM/stript.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// Seja muito bem vindo, muito bem vindo a mais uma aula do nosso curso e agora vamos falar de qual eu
+// devo usar para selecionar meus elementos dentro do HTML com o DOM.
+// No caso, eu gosto muito desses dois abaixo aqui que eu quero ser Lector e o 40 Electoral, porque eles
+// me devolvem um nome de list e aí eu consigo alterar a utilizar as coisas novas do Mac e tudo mais.
+// E vamos ser bem sincero, muito mais fácil utilizar o Core Selector para selecionar uma classe, uma
+// tag 1D.
+// Tudo como se fosse uma sintaxe única do que eu tenho que ficar utilizando o extremamente bad class name,
+// team nm, etc.
+// Então eu gosto muito de utilizar esses dois porque eles são cirurgico no que eu preciso.
+// Esse aqui me retorna um novo de no caso apenas esse.
+// Daqui não de cima esse aqui me retorna o nome de lixo e eu consigo utilizar com fora de série, mas
+// os outros vêm de novo mesmo com Train ou class name que me traz um HTML Collection, eu não gosto utilizar
+// muito eles, alguns vão utilizar mais esses dois, não que sejam errado, porém eu utilizo bem pouco
+// eles e olhe lá se utilizo quando utilizo.
+// Então fica a dica aí para você utilizar esses dois.
+// Mas se você pegar um código que tenha isso, também não está errado.
+// Tá bom, então fica tranquilo para costume.
+// Veja com o que você acha melhor para utilizar, mas fica a dica com esses dois caras dá um forte abraço,
+// fica com Deus até a próxima, foi?
+
+
+// Mais qual devo usar?
+
+// Gosto muito desse dois
+// ------------------------------
+// querySelector
+// querySelectorAll = NodeList
+// ------------------------------
+// getElementById
+// getElementsByCassName
+// getElementsByTagName
+
+
+
+
