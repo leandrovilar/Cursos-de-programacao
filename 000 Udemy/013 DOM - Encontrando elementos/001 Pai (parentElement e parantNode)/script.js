1f476f64d4019eee9a774b5d13de534473989004
Leandro Vilar <leandrovilar@outlook.com>
2024-03-18 18:31:26 -0300

    parentElement e parentNode no DOM

---
+++ 000 Udemy/013 DOM - Encontrando elementos/001 Pai (parentElement e parantNode)/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const strong = document.querySelector("strong");
+const p = document.querySelector(".paragrago");
+const container = document.querySelector(".container");
+const body = document.querySelector("body")
+
+console.log(body.parentElement);
+console.log(body.parentNode);
