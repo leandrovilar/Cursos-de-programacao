5404409fb8450b86f5bb6c49c3887d4ace419f0f
Leandro Vilar <leandrovilar@outlook.com>
2024-03-18 15:15:06 -0300

    Manipuacao de Style e DOM

---
+++ 000 Udemy/012 DOM - Manipulacao de style/001 Style/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const h1 = document.querySelector("h1");
+
+h1.style.fontFamily = "Arial";
+h1.style.color = "red";
+
+const p = document.querySelector("p");
+
+p.forEach((elemet) => {
+    elemet.style.fontFamily = "Arial";
+    elemet.style.fontFamily = "12px";
+
+});
