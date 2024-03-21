c1ce3497668339de7ab8fd27f71267f26ad60562
Leandro Vilar <leandrovilar@outlook.com>
2024-03-21 14:36:14 -0300

    append e appendChild

---
+++ 000 Udemy/013 DOM - Encontrando elementos/007 append e appendChild/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const container = document.querySelector(".containe");
+
+//container.append("Olá Leadnro") // só server para texto
+
+const newDiv = document.createElement("div");
+newDiv.innerText = "Ola Leandro";
+
+container.appendChild(newDiv);
+
