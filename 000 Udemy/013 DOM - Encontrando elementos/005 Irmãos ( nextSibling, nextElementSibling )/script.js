f2b91758f9bf06a6ced8391e20b5687f85a39108
Leandro Vilar <leandrovilar@outlook.com>
2024-03-21 14:05:10 -0300

    up

---
+++ 000 Udemy/013 DOM - Encontrando elementos/005 Irmãos ( nextSibling, nextElementSibling )/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const container = document.querySelector("h1");
+const p = document.querySelector(".paragrafo");
+
+
+console.log(p.nextSibling);
+console.log(p.nextElementSibling);
+
+
