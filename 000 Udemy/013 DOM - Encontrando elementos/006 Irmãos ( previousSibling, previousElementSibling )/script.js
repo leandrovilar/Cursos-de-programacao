07c2a870a35579c85ae72f56de0f62695eae54bb
Leandro Vilar <leandrovilar@outlook.com>
2024-03-21 14:04:12 -0300

    Irmãos (previousSibling, previousElementSibling)

---
+++ 000 Udemy/013 DOM - Encontrando elementos/006 Irmãos ( previousSibling, previousElementSibling )/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const h1 = document.querySelector("h1");
+const p = document.querySelector(".paragrafo");
+
+
+console.log(p.previousSibling);
+console.log(p.previousElementSibling);
+
+
