23e63edeeb539985f2eee6bf076defc2c66dd179
Leandro Vilar <leandrovilar@outlook.com>
2024-03-21 13:43:54 -0300

    firstChild, firstElementChild (filho)

---
+++ 000 Udemy/013 DOM - Encontrando elementos/003 firstChild, firstElementChild (filho)/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const container = document.querySelector(".container");
+const p = document.querySelector(".paragrafo");
+
+//console.log(container.firsChild);
+console.log(p.firstChild);
+console.log(p.firstElementChild);
+
+
