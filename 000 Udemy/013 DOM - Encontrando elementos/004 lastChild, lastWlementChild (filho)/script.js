cffbc477e389295b9bafc70800fd07ae9caa8c79
Leandro Vilar <leandrovilar@outlook.com>
2024-03-21 13:49:51 -0300

    lastChild, lastWlementChild (filho)

---
+++ 000 Udemy/013 DOM - Encontrando elementos/004 lastChild, lastWlementChild (filho)/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const container = document.querySelector(".container");
+const p = document.querySelector(".paragrafo");
+
+
+console.log(p.lastChild);
+console.log(p.lastElementChild);
+
+
