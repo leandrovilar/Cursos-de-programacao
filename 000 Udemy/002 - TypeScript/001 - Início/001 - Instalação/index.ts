6e9c292b130cb00efbb78d0d0e12b9e013f5e575
Leandro Vilar <leandrovilar@outlook.com>
2024-04-02 17:10:16 -0300

    up

---
+++ 000 Udemy/002 - TypeScript/001 - Início/001 - Instalação/index.ts
@@ -0,0 +1,9 @@
+// Typescript
+// npm install -g typescript
+// tsc --init
+
+// plugin: Code runner
+// npm i -g ts-node
+// ctrl + alt + n
+
+console.log ("npm install -g typescript");
--- 000 Udemy/002 TypeScript/001 - Início/001 - Instalação/index.ts
+++
@@ -1,9 +0,0 @@
-// Typescript
-// npm install -g typescript
-// tsc --init
-
-// plugin: Code runner
-// npm i -g ts-node
-// ctrl + alt + n
-
-console.log ("npm install -g typescript");
