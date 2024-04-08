3475e569cfc399dff9f5a02df3c83cf6240eba67
Leandro Vilar <leandrovilar@outlook.com>
2024-04-08 11:21:31 -0300

    Tipos de typescript

---
+++ 000 Udemy/002 - TypeScript/002 - Tipos/index.ts
@@ -0,0 +1,13 @@
+let pedido: string = "Pissa";
+pedido = "Leandro Vilar";
+
+let numero: number = 123;
+numero = 321;
+
+let deuBom: boolean = false;
+
+const fun = (value: string): string => {
+    return value;
+}
+
+console.log(fun("Leandro Vilar"));
