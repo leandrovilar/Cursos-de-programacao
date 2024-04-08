2b3680c21457f676d97ac6b88662c4f72d189274
Leandro Vilar <leandrovilar@outlook.com>
2024-04-08 10:38:09 -0300

    Instalanco o typescript

---
+++ 000 Udemy/002 - TypeScript/001 - Início/001 - Instalação/index.js
@@ -0,0 +1,7 @@
+// Typescript
+// npm install -g typescript
+// tsc --init
+// plugin: Code runner
+// npm i -g ts-node
+// ctrl + alt + n
+console.log("npm install -g typescript");
--- 000 Udemy/002 - TypeScript/001 - Início/001 - Instalação/index.ts
+++ 000 Udemy/002 - TypeScript/001 - Início/001 - Instalação/index.ts
@@ -7,3 +7,9 @@
 // ctrl + alt + n
 
 console.log ("npm install -g typescript");
+
+const pedido = (pedido: string) => {
+    return `Deu bom o meu pedido ${pedido}`;
+};
+
+console.log (pedido("1234"))
