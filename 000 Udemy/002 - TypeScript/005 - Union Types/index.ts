af7682e00426085873bbc8f771b59dc771b02772
Leandro Vilar <leandrovilar@outlook.com>
2024-04-08 12:12:03 -0300

    Union Types | no Typescript

---
+++ 000 Udemy/002 - TypeScript/005 - Union Types/index.ts
@@ -0,0 +1,5 @@
+let value: string | boolean = "leandro"
+
+value = "leandro"; // valor aceito tipo string
+value = false; // vlor aceito tipo boolean
+// value = 123; // valor nao aceito tipo number
