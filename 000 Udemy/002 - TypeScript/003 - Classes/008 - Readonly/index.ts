647168eba07fdbae9175d03d4933ea6b5179f84d
Leandro Vilar <leandrovilar@outlook.com>
2024-04-10 11:27:35 -0300

    up

--- 000 Udemy/002 - TypeScript/003 - Classes/008 - Readonly/index.ts
+++ 000 Udemy/002 - TypeScript/003 - Classes/008 - Readonly/index.ts
@@ -14,6 +14,7 @@ console.log(pessoa.nome);
 
 // Em TypeScript, a palavra-chave `readonly` é usada para definir propriedades de um objeto que não podem ser modificadas após sua inicialização. Isso significa que uma vez que o valor de uma propriedade é atribuído, ele não pode ser alterado posteriormente.
 
+/*
 // Aqui está um exemplo simples do chatGPT:
 class Pessoa {
     readonly nome: string;
@@ -28,5 +29,7 @@ console.log(pessoa.nome); // Saída: João
 
 // Tentativa de modificar o nome, resultará em um erro
 pessoa.nome = "Maria"; // Erro: Cannot assign to 'nome' because it is a read-only property
+
+*/
 
 // Neste exemplo, a propriedade `nome` da classe `Pessoa` é declarada como `readonly`, o que significa que seu valor só pode ser atribuído uma vez, dentro do construtor da classe. Qualquer tentativa de modificá-lo fora do construtor resultará em um erro de compilação. Isso é útil quando você quer garantir que certos valores não sejam alterados após a inicialização do objeto, o que pode ajudar a evitar bugs difíceis de rastrear em seu código.
