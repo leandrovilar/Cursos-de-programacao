7ddbaa151dae5d8d189b8391e8c7ec8402e6f052
Leandro Vilar <leandrovilar@outlook.com>
2024-04-11 16:06:53 -0300

    components no Angular

--- 001 Udemy/003 - Angular/meu-primeiro-projeto-latest/src/app/app.component.ts
+++ 001 Udemy/003 - Angular/meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NewComponent } from './components/new-component/new-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CommonModule, RouterOutlet, NewComponent],
+  template: `
+  <!-- <router-outlet></router-outlet> -->
+  <h1>Curso de Angular</h1>
+  <app-new-component />
+`,
 })
 export class AppComponent {
   title = 'meu-primeiro-projeto-latest';
