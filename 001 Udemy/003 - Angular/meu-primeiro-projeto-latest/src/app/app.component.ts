1771c8952a275b2d7b8136548a2592ab58d52f14
Leandro Vilar <leandrovilar@outlook.com>
2024-04-11 17:00:00 -0300

    Estilos css no Angular

--- 001 Udemy/003 - Angular/meu-primeiro-projeto-latest/src/app/app.component.ts
+++ 001 Udemy/003 - Angular/meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -10,7 +10,9 @@ import { NewComponent } from './components/new-component/new-component.component
   template: `
   <!-- <router-outlet></router-outlet> -->
   <h1>Curso de Angular</h1>
+  <div class="theme-dark"
   <app-new-component />
+  </div>
 `,
 })
 export class AppComponent {
