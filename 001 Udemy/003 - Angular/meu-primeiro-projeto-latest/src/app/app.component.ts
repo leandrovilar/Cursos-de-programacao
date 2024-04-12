0b5d1e14e2dff05f7eb44479d0e3f82e2fdbadf2
Leandro Vilar <leandrovilar@outlook.com>
2024-04-12 10:40:42 -0300

    Text Interpolation no Angular

--- 001 Udemy/003 - Angular/meu-primeiro-projeto-latest/src/app/app.component.ts
+++ 001 Udemy/003 - Angular/meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+//components
 import { NewComponent } from './components/new-component/new-component.component';
+import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NewComponent],
+  imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent],
   template: `
-  <!-- <router-outlet></router-outlet> -->
+  // <router-outlet></router-outlet>
+  // <app-new-component />
   <h1>Curso de Angular</h1>
-  <div class="theme-dark"
-  <app-new-component />
-  </div>
-`,
+  <app-template-binding />
+  `,
 })
 export class AppComponent {
   title = 'meu-primeiro-projeto-latest';
