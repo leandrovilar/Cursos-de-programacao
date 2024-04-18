9db27ce477e5ba15f7588230e7a79f2968daab2e
Leandro Vilar <leandrovilar@outlook.com>
2024-04-18 19:48:47 -0300

    Template Event binding

--- 001 Udemy/003 - Angular/meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.component.ts
+++ 001 Udemy/003 - Angular/meu-primeiro-projeto-latest/src/app/components/template/template-binding/template-binding.component.ts
@@ -10,14 +10,35 @@ import { Component } from '@angular/core';
 })
 export class TemplateBindingComponent {
 public name = 'Leandro Vilar';
-public age = 44;
+public age = 42;
 public condition = this.age > 1 ? 'Teste' : 'Teste2';
 
 public isDisabled = true;
 public srcValue = 'https://blog.logrocket.com/wp-content/uploads/2021/04/Angular-in-memory-web-api-tutorial.jpg'
 
 //codificacao so para teste
-public sum(val1: number, val2: number){
-  return val1 + val2;
+// public sum(val1: number, val2: number){
+//   return val1 + val2;
+// }
+public isTextDecoration = this.age >= 44 ? 'underline' : 'none';
+
+public sum(){
+  return this.age++;
 }
+
+public sub(){
+  return this.age--;
+}
+
+public onkeyDown(event: Event){
+  return console.log(event);
+}
+public onMouseMove(event:MouseEvent){
+  return console.log({
+    clietX: event.clientX,
+    clietY: event.clientY,
+  });
+}
+
+
 }
