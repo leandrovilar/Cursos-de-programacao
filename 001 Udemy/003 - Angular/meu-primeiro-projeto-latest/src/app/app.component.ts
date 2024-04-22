ce7dfad3570610b3f4d2c55775996e47929bb79e
Leandro Vilar <leandrovilar@outlook.com>
2024-04-22 14:38:09 -0300

    if no control flow do Angular

--- 001 Udemy/003 - Angular/meu-primeiro-projeto-latest/src/app/app.component.ts
+++ 001 Udemy/003 - Angular/meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
+import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
 
 @Component({
   selector: 'app-root',
@@ -15,14 +16,20 @@ import { TemplateVariablesComponent } from './components/template/template-varia
     RouterOutlet,
     NewComponent,
     TemplateBindingComponent,
-    TemplateVariablesComponent],
-  template: `
-  <!-- <router-outlet></router-outlet>  -->
+    TemplateVariablesComponent,
+    TemplateControlFlowComponent,
 
-  <!-- <app-new-component /> -->
+  ],
+
+  template: `
   <h1>Curso de Angular</h1>
+
+  <!-- <router-outlet></router-outlet>  -->
+  <!-- <app-new-component /> -->
   <!--<app-template-binding /> -->
-  <app-template-variables/>
+  <!--<app-template-variables/> -->
+  <app-template-control-flow/>
+
   `,
 })
 export class AppComponent {
