3691a8921c10cd2237e6d40d2a6297faa41a80e3
Leandro Vilar <leandrovilar@outlook.com>
2024-04-22 13:36:18 -0300

    Template Variable no Angular

--- 001 Udemy/003 - Angular/meu-primeiro-projeto-latest/src/app/app.component.ts
+++ 001 Udemy/003 - Angular/meu-primeiro-projeto-latest/src/app/app.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
 //components
 import { NewComponent } from './components/new-component/new-component.component';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
+import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NewComponent, TemplateBindingComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NewComponent,
+    TemplateBindingComponent,
+    TemplateVariablesComponent],
   template: `
   <!-- <router-outlet></router-outlet>  -->
-  <!-- <app-new-component />  -->
+
+  <!-- <app-new-component /> -->
   <h1>Curso de Angular</h1>
-  <app-template-binding />
+  <!--<app-template-binding /> -->
+  <app-template-variables/>
   `,
 })
 export class AppComponent {
