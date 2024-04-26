bf5febaef07255326594d9c135c78ff87c28fdcc
Leandro Vilar <leandrovilar@outlook.com>
2024-04-26 09:33:05 -0300

    FOR e seus valores do Contro Flow do

--- 001 Udemy/003 - Angular/meu-primeiro-projeto-latest/src/app/components/template/template-control-flow/template-control-flow.component.ts
+++ 001 Udemy/003 - Angular/meu-primeiro-projeto-latest/src/app/components/template/template-control-flow/template-control-flow.component.ts
@@ -1,14 +1,29 @@
 import { AsyncPipe, CommonModule, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { Observable, delay, of } from 'rxjs';
 
 @Component({
   selector: 'app-template-control-flow',
   standalone: true,
   imports: [AsyncPipe, NgIf, NgFor, CommonModule],
   templateUrl: './template-control-flow.component.html',
-  styleUrl: './template-control-flow.component.scss'
+  styleUrl: './template-control-flow.component.scss',
 })
 export class TemplateControlFlowComponent {
   public isTrue = true;
 
+  public itens = [{ name: 'Leandro Braga' }];
+  public loadingData$: Observable<string[]> = of([
+    'item 1',
+    'item 2',
+    'item 3',
+  ]).pipe(delay(3000));
+
+  public trackByFn(index: number) {
+    return index;
+  }
+public addNewName(value: string){
+  return this.itens.push({name: value});
 }
+}
+
