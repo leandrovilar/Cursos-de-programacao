68563d11d29beb82784f25482318625477401f50
Leandro Vilar <leandrovilar@outlook.com>
2024-06-24 14:13:46 -0300

    Java basico

--- 02 DIO/03 - Java/trilha-java-basico/java-anatomia-classes/src/MinhaClasse.java
+++ 02 DIO/03 - Java/trilha-java-basico/java-anatomia-classes/src/MinhaClasse.java
@@ -1,6 +1,17 @@
 public class MinhaClasse {
 
 public static void main (String [] args){
-    System.out.print ("Olá turma");
+  
+    String primeiroNome = "Leandro";
+    String segundoNome = "Vilar";
+    String nomeCompleto =  nomeCompleto(primeiroNome, segundoNome);
+
+    System.out.println(nomeCompleto);
+
 }
+
+public static String nomeCompleto (String primeiroNome, String segundoNome){
+    return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+}
+
 }
