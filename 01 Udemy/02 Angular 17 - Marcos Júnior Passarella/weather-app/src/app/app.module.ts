8b7205a5dca92daef1e1df0207836ad36e14a485
Leandro Vilar <leandrovilar@outlook.com>
2024-07-30 15:06:17 -0300

    WeatherService

--- 01 Udemy/02 Angular 17 - Marcos Júnior Passarella/weather-app/src/app/app.module.ts
+++ 01 Udemy/02 Angular 17 - Marcos Júnior Passarella/weather-app/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherHomeComponent } from './modules/wheater/page/weather-home/weather-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherHomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ 01 Udemy/02 Angular 17 - Marcos Júnior Passarella/weather-app/src/app/modules/wheater/services/weather.service.ts
@@ -0,0 +1,19 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  private apiKey = '6382bcc26479515eedc5eecb7474992f';
+
+  constructor(private http: HttpClient) {}
+
+  getWeatherDatas(cityName: string): Observable<any>{
+    return this.http.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&unitsmetric&mode=json&appid=${this.apiKey}`,
+      {}
+    )
+  }
+}
