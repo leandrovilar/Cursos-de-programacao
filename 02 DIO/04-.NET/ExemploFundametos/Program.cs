bdf3119e127cd12777f5496b5c92578160332552
Leandro Vilar <leandrovilar@outlook.com>
2024-09-10 15:25:38 -0300

    Tipos de variaveis

--- 02 DIO/04-.NET/ExemploFundametos/Program.cs
+++ 02 DIO/04-.NET/ExemploFundametos/Program.cs
@@ -1,10 +1,22 @@
 ﻿using ExemploFundametos.Models;
 
-Pessoa p = new Pessoa
-{
-    Nome = "Leandro",
-    Idade = 44
-};
+string apresentacao = "Olá, seja bem vindo"; 
+int quantidade = 1;
+double altura = 1.80;
+decimal preco = 1.80m;
+bool condicao = true;
 
-p.Apresentar();
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variável quantidade: "+ quantidade);
+Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+Console.WriteLine("Valor da variável preço: " + preco);
+Console.WriteLine("Valor da variável condicao: " +condicao );
+
+
+// Pessoa p = new Pessoa
+// {
+//     Nome = "Leandro",
+//     Idade = 44
+// };
+// p.Apresentar();
 
