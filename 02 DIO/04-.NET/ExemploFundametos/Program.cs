39743e462f2381da65038f20c471bcf861e49c79
Leandro Vilar <leandrovilar@outlook.com>
2024-09-11 09:38:44 -0300

    Tipo de DataTime

--- 02 DIO/04-.NET/ExemploFundametos/Program.cs
+++ 02 DIO/04-.NET/ExemploFundametos/Program.cs
@@ -1,15 +1,25 @@
 ﻿using ExemploFundametos.Models;
 
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+
+
+
+
+
+
+
+
+
+
+
 //string apresentacao = "Olá, seja bem vindo"; 
 
-int quantidade = 1;
+// int quantidade = 1;
+// Console.WriteLine("Valor da variável quantidade: "+ quantidade);
 
-Console.WriteLine("Valor da variável quantidade: "+ quantidade);
-
-quantidade = 10;
-
-Console.WriteLine("Valor da variável quantidade: "+ quantidade);
-
+// quantidade = 10;
+// Console.WriteLine("Valor da variável quantidade: "+ quantidade);
 
 // double altura = 1.80;
 // decimal preco = 1.80m;
--- 02 DIO/04-.NET/ExemploFundametos/obj/Debug/net8.0/ExemploFundametos.AssemblyInfo.cs
+++ 02 DIO/04-.NET/ExemploFundametos/obj/Debug/net8.0/ExemploFundametos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploFundametos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ad598d5deba7a8aeb45de2605b4220970335c39")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+413744b9e854ba7deb94197293e9cd398b09f997")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploFundametos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploFundametos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
