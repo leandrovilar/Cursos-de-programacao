413744b9e854ba7deb94197293e9cd398b09f997
Leandro Vilar <leandrovilar@outlook.com>
2024-09-11 09:32:52 -0300

    Manipulação de variaveis

--- 02 DIO/04-.NET/ExemploFundametos/Program.cs
+++ 02 DIO/04-.NET/ExemploFundametos/Program.cs
@@ -1,16 +1,25 @@
 ﻿using ExemploFundametos.Models;
 
-string apresentacao = "Olá, seja bem vindo"; 
-int quantidade = 1;
-double altura = 1.80;
-decimal preco = 1.80m;
-bool condicao = true;
+//string apresentacao = "Olá, seja bem vindo"; 
 
-Console.WriteLine(apresentacao);
+int quantidade = 1;
+
 Console.WriteLine("Valor da variável quantidade: "+ quantidade);
-Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
-Console.WriteLine("Valor da variável preço: " + preco);
-Console.WriteLine("Valor da variável condicao: " +condicao );
+
+quantidade = 10;
+
+Console.WriteLine("Valor da variável quantidade: "+ quantidade);
+
+
+// double altura = 1.80;
+// decimal preco = 1.80m;
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variável quantidade: "+ quantidade);
+// Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+// Console.WriteLine("Valor da variável preço: " + preco);
+// Console.WriteLine("Valor da variável condicao: " +condicao );
 
 
 // Pessoa p = new Pessoa
