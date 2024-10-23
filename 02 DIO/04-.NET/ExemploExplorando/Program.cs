804a0ea1be8e493ba8551bcc8ad20fccc07eb5e6
Leandro Vilar <leandrovilar@outlook.com>
2024-10-23 11:39:18 -0300

    Usando Throw

--- 02 DIO/04-.NET/ExemploExplorando/Program.cs
+++ 02 DIO/04-.NET/ExemploExplorando/Program.cs
@@ -1,34 +1,59 @@
-﻿using ExemploExplorando.Models;
+﻿using ExemploExplorando;
+using ExemploExplorando.Models;
 using System.Data;
 using System.Globalization; //Alterando a localização do código
 
-//tratamento de exceção
-try
-{
-    string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");
+new ExemploExcecao().Metodo1();
 
-    foreach (string linha in linhas)
-    {
-        Console.WriteLine(linha);
-    }
-}
-catch (FileNotFoundException ex)
-{
-    Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Arquivo não encontrado. {ex.Message}");
-}
-catch (DirectoryNotFoundException ex)
-{
-    Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Caminho da pasta não encontrado."  + ex.Message);
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
-}
-//Bloco finally (sempre aparece no final de um execução de código)
-finally
-{
-    Console.WriteLine("Chegou até aqui");
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //tratamento de exceção
+// try
+// {
+//     string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");
+
+//     foreach (string linha in linhas)
+//     {
+//         Console.WriteLine(linha);
+//     }
+// }
+// catch (FileNotFoundException ex)
+// {
+//     Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Arquivo não encontrado. {ex.Message}");
+// }
+// catch (DirectoryNotFoundException ex)
+// {
+//     Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Caminho da pasta não encontrado."  + ex.Message);
+// }
+// catch (Exception ex)
+// {
+//     Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
+// }
+// //Bloco finally (sempre aparece no final de um execução de código)
+// finally
+// {
+//     Console.WriteLine("Chegou até aqui");
+// }
 
 
 
