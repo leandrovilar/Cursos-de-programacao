b8be8bd8e1b1da4628218eb27de569b926d236bf
Leandro Vilar <leandrovilar@outlook.com>
2025-01-17 17:02:54 -0300

    teste de lógica

---
+++ Logica/Program.cs
@@ -0,0 +1,44 @@
+﻿
+//-----> Váriaveis
+// int idade;           // Variável para armazenar idade
+// string nome;         // Variável para armazenar nome
+// bool maiorDeIdade;   // Variável para indicar se é maior de idade
+
+
+//-----> Estrutura geral de um programa
+// using System;
+// //Início
+// class Program{
+//     static void Main(String[] args){
+
+//         //Declaraçao de variaveis
+//         int n1, n2, soma;
+
+//         //Entrada de dados
+//         Console.WriteLine("Digite o primeiro número:");
+//         n1 = int.Parse(Console.ReadLine());
+
+//         Console.WriteLine("Digite o segundo número:");
+//         n2 = int.Parse(Console.ReadLine());
+
+//         //Processamento de dados
+//         soma = n1 + n2;
+
+//         //Saida de dados
+//         Console.WriteLine("O número é: " + soma);
+//     }
+// }
+// // Fim
+
+
+//-----> Algoritimo básico para verificar se um número é par ou ímpar
+// int numero = 18;
+
+// if (numero % 2 == 0)
+// {
+//     Console.WriteLine("O número e par");
+// }
+// else
+// {
+//     Console.WriteLine("O número é impar");
+// }
