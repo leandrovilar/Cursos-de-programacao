8e7d78a24093dd1af56e24ec30d2e1913b2fef25
Leandro Vilar <leandrovilar@outlook.com>
2025-02-13 10:24:13 -0300

    Up

--- DIO/.NET/ExemploFundamentos/ExemploFundamentos.Common/Models/Pessoa.cs
+++ DIO/.NET/ExemploFundamentos/ExemploFundamentos.Common/Models/Pessoa.cs
@@ -5,18 +5,16 @@ using System.Threading.Tasks;
 
 namespace ExemploFundamentos.Common.Models
 {
-   /// <summary>
-   /// Representa uma pessoa física
-   /// </summary>
-    public class Pessoa
+  
+   // Representa uma pessoa física
+     public class Pessoa
     {
+       // Caracteristica da pessoa(dados)
         public required string Nome { get; set; }
         public int Idade { get; set; }
 
-        /// <summary>
-        /// Faz a pessoa se apresentar
-        /// </summary>
-        public void Apresentar()
+         // Faz a pessoa se apresentar(Metodos (função))
+            public void Apresentar()
         {
             Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos");
         }
