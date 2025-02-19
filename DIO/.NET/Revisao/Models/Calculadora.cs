a2576907225827460e58be90e25ad92252522dbf
Leandro Vilar <leandrovilar@outlook.com>
2025-02-19 09:45:50 -0300

    Calculadora completa

--- DIO/.NET/Revisao/Models/Calculadora.cs
+++ DIO/.NET/Revisao/Models/Calculadora.cs
@@ -23,5 +23,33 @@ namespace Revisao.Models
         {
             Console.WriteLine($"{x} / {y} = {x / y}");
         }
+        public void Potencia(int x, int y)
+        {
+            double pot = Math.Pow(x, y);
+            Console.WriteLine($"{x}^{y} = {pot}");
+        }
+        public void Seno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double seno = Math.Sin(radiano);
+            Console.WriteLine($"Seno de {angulo} = {Math.Round(seno, 5)}");
+        }
+        public void Coseno(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double coseno = Math.Cos(radiano);
+            Console.WriteLine($"Coseno de {angulo} = {Math.Round(coseno, 5)}");
+        }
+        public void Tangente(double angulo)
+        {
+            double radiano = angulo * Math.PI / 180;
+            double tangente = Math.Tan(radiano);
+            Console.WriteLine($"Tangente de {angulo} = {Math.Round(tangente, 5)}");
+        }
+        public void RaizQuadrada(double x)
+        {
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine($"Raiz quadrada de {x} = {raiz}");
+        }
     }
 }
--- DIO/.NET/Revisao/Program.cs
+++ DIO/.NET/Revisao/Program.cs
@@ -2,7 +2,13 @@
 
 Calculadora calc = new Calculadora();
 
-calc.Somar(10, 20);
-calc.Subtrair(10, 20);
-calc.Multiplicar(10, 20);
-calc.Dividir(10, 20);
+// calc.Somar(10, 20);
+// calc.Subtrair(10, 20);
+// calc.Multiplicar(10, 20);
+// calc.Dividir(10, 20);
+
+// calc.Potencia(3, 3);
+// calc.Seno(30);
+// calc.Coseno(30);
+// calc.Tangente(30);
+calc.RaizQuadrada(9);
