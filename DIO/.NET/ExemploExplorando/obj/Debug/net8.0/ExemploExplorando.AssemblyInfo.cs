40255b3996adde6212cff5fa9923729df736d720
Leandro Vilar <leandrovilar@outlook.com>
2025-09-01 16:13:24 -0300

    Up

--- DIO/.NET/ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
+++ DIO/.NET/ExemploExplorando/obj/Debug/net8.0/ExemploExplorando.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94dbe15abfea8d9235d5adfb8337f45b8ff02406")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+781ce795e3535fdeed58851d76a406a97fe998b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploExplorando")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
