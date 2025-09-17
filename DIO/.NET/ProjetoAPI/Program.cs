00c54ad192e86838c7b37f7879130bfa7bbab4c5
Leandro Vilar <leandrovilar@outlook.com>
2025-09-17 09:40:22 -0300

    Organizando rotas por contexto no Swagger

--- DIO/.NET/ProjetoAPI/Program.cs
+++ DIO/.NET/ProjetoAPI/Program.cs
@@ -29,7 +29,7 @@ var app = builder.Build();
 #endregion
 
 #region Home
-app.MapGet("/", () => Results.Json(new Home()));
+app.MapGet("/", () => Results.Json(new Home())).WithTags("Home");
 #endregion
 
 #region Administradores
@@ -39,7 +39,7 @@ app.MapPost("Administradores/login", ([FromBody] LoginDTO loginDTO, IAdministrad
         return Results.Ok("Login com sucesso");
     else
         return Results.Unauthorized();
-});
+}).WithTags("Administradores");
 #endregion
 
 #region Veiculos
@@ -53,13 +53,13 @@ app.MapPost("/veiculos", ([FromBody] VeiculoDTO veiculoDTO, IVeiculoServico veic
     };
     veiculoServico.Incluir(veiculo);
     return Results.Created($"/veiculo/{veiculo.Id}", veiculo);
-});
+}).WithTags("Veiculos");
 
 app.MapGet("/veiculos", ([FromQuery] int? pagina, IVeiculoServico veiculoServico) =>
 {
     var veiculos = veiculoServico.Todos(pagina);
     return Results.Ok(veiculos);
-});
+}).WithTags("Veiculos");
 
 #endregion
 
