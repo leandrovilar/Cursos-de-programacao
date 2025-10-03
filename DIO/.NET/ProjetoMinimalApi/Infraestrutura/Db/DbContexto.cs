e09079acfe0f9c546948dbfd0685edc7b651b920
Leandro Vilar <leandrovilar@outlook.com>
2025-10-03 09:58:45 -0300

    Criando Seed para cadastrar administrador padrão

--- DIO/.NET/ProjetoMinimalApi/Infraestrutura/Db/DbContexto.cs
+++ DIO/.NET/ProjetoMinimalApi/Infraestrutura/Db/DbContexto.cs
@@ -13,6 +13,20 @@ public class DbContexto : DbContext
 
 
     public DbSet<Administrador> Administradores { get; set; } = default!;
+
+    //Criando o Seed para cadatrar administrador padrao 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Administrador>().HasData(new Administrador
+        {
+            Id = 1,
+            Email = "administrador@teste.com",
+            Senha = "123456",
+            Perfil = "Adm"
+        }
+        );
+    }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         if (!optionsBuilder.IsConfigured)
