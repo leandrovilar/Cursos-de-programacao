a2e45d87d49bda852aea0adf7f141010a3831aa7
Leandro Vilar <leandrovilar@outlook.com>
2025-10-22 17:35:31 -0300

    Configurar o Program.cs

--- DIO/.NET/Desafio/ProjetoGerenciadorVeiculos/Api/Program.cs
+++ DIO/.NET/Desafio/ProjetoGerenciadorVeiculos/Api/Program.cs
@@ -1,1 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetoGerenciadorVeiculos.Infraestrutura.Db;
+using ProjetoGerenciadorVeiculos.Dominio.Entidades;
 
+var builder = WebApplication.CreateBuilder(args);
+
+// Configurar conexão MySQL
+builder.Services.AddDbContext<DbContexto>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("MySql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("MySql"))
+    )
+);
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/", () => "🚗 API de Gerenciamento de Veículos rodando com .NET 8!");
+
+// Endpoint de teste inicial
+app.MapGet("/usuarios", async (DbContexto db) => await db.Usuarios.ToListAsync());
+
+app.Run();
