bca82b030c7a9f305b164d189e411b643b6588bf
Leandro Vilar <leandrovilar@outlook.com>
2025-10-31 18:45:33 -0300

    Projeto Gerenciador de Veiculos (sem testes)

--- DIO/.NET/Desafio/ProjetoGerenciadorVeiculos/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ DIO/.NET/Desafio/ProjetoGerenciadorVeiculos/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b01ec0b2e6d668ee7cf2c861a777788d9aaed533")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3033f97b83b6ef292c104b6c18d6d5dd75c88a81")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
